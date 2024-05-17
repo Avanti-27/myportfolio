a659a9aafd69ca090b15ed8464fcc377eccdda3f
Avanti <avanti.tatwawadi@gmail.com>
2024-05-18 00:42:44 +0530

    My Portfolio - Commit 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
       className="p-5 my-0 mx-auto bg-gradient-to-r from-sky-500 to-indigo-500 w-[1000px] text-center rounded-xl mb-10 shadow-2xl shadow-black"
       id="contact"
     >
-      <a href="mailto:example@gmail.com">
+      <a href="mailto:avanti.tatwawadi@gmail.com">
         <button
           type="button"
           className="font-bold font-serif text-xl text-white"
